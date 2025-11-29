5e9a487f73504893be1e8e055449375fa1b3435e
Nurfarzana <nnbmohdjohari1@sheffield.ac.uk>
2025-11-29 22:22:55 +0000

    Add deployment configuration

--- server_flask.py
+++ server_flask.py
@@ -189,5 +189,6 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+if __name__ == '__main__':
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
